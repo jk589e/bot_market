c06949b0486437ca8cd7172f9ad83e93f7326e5b
jk58e9 <jk58e91@gmail.com>
2022-05-09 19:23:23 +0300

    new models

--- market_site/telegramapi/views.py
+++ market_site/telegramapi/views.py
@@ -49,6 +49,6 @@ class UpdateBot(APIView):
 
         
 
-        api_telegram.send_message(chat_id=526697170, message=str(dfMain.iloc[0,8]))
+        api_telegram.send_message(chat_id=dfMain.iloc[0,7], message=str(dfMain.iloc[0,9]))
 
         return Response({'code': 200})
