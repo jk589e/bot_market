117b5609d68e9c4defd7fe01f2c0a2fa6337ac88
jk58e9 <jk58e91@gmail.com>
2022-07-05 15:30:16 +0300

    add gift

--- market_site/market/models.py
+++ market_site/market/models.py
@@ -59,6 +59,11 @@ class User(models.Model):
             return mark_safe('<img src="{0}" width="150" height="150" />'.format(self.user_photo.url))
         else:
             return "(no photo)"
+    def photo_basket(self):
+        if self.user_photo:
+            return mark_safe('<img src="{0}" height="100" />'.format(self.user_photo.url))
+        else:
+            return "(no photo)"
 
 class TelegramLog(models.Model):
     update_id = models.BigIntegerField()
